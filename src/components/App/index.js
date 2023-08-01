8113e4ca7a4796be0dbdfdaa812048b9cf041dee
Konstantin Lekh <>
2023-08-02 01:02:27 +0200

    cleanup

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,11 +4,8 @@ import {Player} from "../Player";
 import {Footer} from "../Footer";
 
 export function App() {
-  const standalone = document.location.pathname !== "/embed";
-
   return (
     <div className="app">
-      {standalone && <Header/> }
       <div className="main">
         <Player/>
       </div>
