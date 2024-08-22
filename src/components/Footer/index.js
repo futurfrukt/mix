3ce4bb2c759b6d421a535183267681b5220efd1a
K. Lekh <aheado.nl@gmail.com>
2024-08-22 17:11:44 +0200

    changed domain

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import './index.css';
 export const Footer = () => {
     return (<div className="footer">
         <div className="disclaimer">
-            Made with <span>🖤</span> by <a href="https://lekh.nl" target="_blank">K. Lekh</a>
+            Made with <span>🖤</span> by <a href="https://lekh.live" target="_blank">K. Lekh</a>
         </div>
     </div>);
 }
